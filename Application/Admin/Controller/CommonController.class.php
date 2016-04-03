d31996f63356ae2f61e7bb735051ec64c818b09e
kongxu633 <kongxu633@gmail.com>
2016-04-03 17:29:39 +0800

    公共检测

---
+++ Application/Admin/Controller/CommonController.class.php
@@ -0,0 +1,10 @@
+<?php
+namespace Admin\Controller;
+use Think\Controller;
+class CommonController extends Controller {
+    public function __construct(){
+        parent::__construct();
+        echo 1;
+  
+    }
+}
