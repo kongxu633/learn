de389f4ff2867c2a4ffd85133f5535f798ef1074
kongxu633 <kongxu633@gmail.com>
2016-04-08 10:07:53 +0800

    显示分类中图片

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -19,6 +19,13 @@ class IndexController extends Controller {
         $cate = M('cate')->where(array('id'=>$cid))->find();
         $this->assign('cate',$cate);
         
+        $pic = [
+            ['name'=>'http://img.alicdn.com/tps/i3/TB1kt4wHVXXXXb_XVXX0HY8HXXX-1024-1024.jpeg'],
+            ['name'=>'http://img.alicdn.com/tps/i1/TB1SKhUHVXXXXb7XXXX0HY8HXXX-1024-1024.jpeg'],
+            ['name'=>'http://img.alicdn.com/tps/i4/TB1AdxNHVXXXXasXpXX0HY8HXXX-1024-1024.jpeg'],
+        ];
+        $this->assign('pic',$pic);
+        
         $art = D('article')->getCateArticle($cid);
         $this->assign('art',$art);
         
